660ef59988e484a8216a1a8df0b2bc03e8c9c304
JoaoRudolf <66043053+JoaoRudolf@users.noreply.github.com>
2022-01-07 20:59:29 -0300

    Update doughnut_list2.sql

--- doughnut_list2.sql
+++ doughnut_list2.sql
@@ -1,5 +1,13 @@
 create table doughnut_list2 (
 	doughnut_name VARCHAR(10) not null,
 	doughnut_type varchar(8) not null,
-	doughtnut_cost dec(3,2) not null
+	doughtnut_cost dec(3,2) not null default 1.0
 );
+
+drop table doughnut_list2;
+
+select * from doughnut_list2;
+
+insert into doughnut_list2 
+(doughnut_name, doughnut_type)
+values ('teste', 'teste2');
