15a4ebf1ad1be305bb572cac12e4c0d1d05543d6
JoaoRudolf <66043053+JoaoRudolf@users.noreply.github.com>
2022-01-07 20:58:49 -0300

    Update contatos.sql

--- contatos.sql
+++ contatos.sql
@@ -1,7 +1,7 @@
 create table meus_contatos (
 	sobrenome varchar(30) not null,
-	nome varchar(20) not null,
-	email varchar(50) not null,
+	nome varchar(20)not null,
+	email varchar(50)not null,
 	data_nasc date not null,
 	profissao varchar(50) not null,
 	localizacao varchar(50) not null,
@@ -14,3 +14,20 @@ select * from meus_contatos;
 
 insert into meus_contatos 
 values ('Pé de Feijão', 'João', 'joaof@gmail.com', '1988-11-24', 'Contador', 'Blumenau', 'Casado', 'programming', 'jobs');
+
+insert into meus_contatos 
+values ('Toth', 'Anne', 'anne_tf@gmail.com', '1990-01-15', 'Scrum Master', 'Blumenau', 'Casada', 'programming', 'money');
+
+insert into meus_contatos 
+values ('Manson', 'Anne', 'a.mansonf@gmail.com', '1992-01-19', 'Dev', 'Blumenau', 'Solteira', 'sports', 'fashion');
+
+select * from meus_contatos 
+where nome = 'Anne';
+
+select nome, email
+from meus_contatos
+where nome < 'b';
+
+select nome, email
+from meus_contatos
+where email like '%anne%';
