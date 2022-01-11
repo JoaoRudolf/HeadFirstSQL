8a789ebbf1768dc0236ada67f924a872615c92a8
JoaoRudolf <66043053+JoaoRudolf@users.noreply.github.com>
2022-01-10 21:37:16 -0300

    Update contatos.sql

--- contatos.sql
+++ contatos.sql
@@ -31,3 +31,35 @@ where nome < 'b';
 select nome, email
 from meus_contatos
 where email like '%anne%';
+
+drop table meus_contatos;
+
+create table meus_contatos (
+	id int not null,
+	sobrenome varchar(30) not null,
+	nome varchar(20)not null,
+	email varchar(50)not null,
+	data_nasc date not null,
+	profissao varchar(50) not null,
+	localizacao varchar(50) not null,
+	status varchar(20) not null,
+	interesses varchar(100) not null,
+	procurando varchar(100) not null,
+	primary key (id)
+);
+
+create table meus_contatos (
+	sobrenome varchar(30) not null,
+	nome varchar(20)not null,
+	email varchar(50)not null,
+	data_nasc date not null,
+	profissao varchar(50) not null,
+	localizacao varchar(50) not null,
+	status varchar(20) not null,
+	interesses varchar(100) not null,
+	procurando varchar(100) not null
+);
+
+alter table meus_contatos 
+add column id int not null,
+add primary key (id);
